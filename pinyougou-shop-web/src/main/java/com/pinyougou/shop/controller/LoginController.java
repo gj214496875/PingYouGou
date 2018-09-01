bd9d845739e8c6ec88f5b883b339e40c1491df0f
guojun <214496875@qq.com>
2018-09-01 08:31:05 +0800

    day04 完成商家和管理登录权限控制，商家申请入驻和管理审核商家功能

---
+++ pinyougou-shop-web/src/main/java/com/pinyougou/shop/controller/LoginController.java
@@ -0,0 +1,24 @@
+package com.pinyougou.shop.controller;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Enzo Cotter on 2018/8/31.
+ */
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+
+    @RequestMapping("/name")
+    public Map getUserName() {
+        String userName = SecurityContextHolder.getContext().getAuthentication().getName();
+        Map map = new HashMap(16);
+        map.put("loginName", userName);
+        return map;
+    }
+}
