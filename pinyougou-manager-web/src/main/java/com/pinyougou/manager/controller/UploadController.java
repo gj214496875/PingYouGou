00642fc054a368bd35bd4ea021449d0c77957b9e
guojun <214496875@qq.com>
2018-09-09 10:50:16 +0800

    day08 完成首页广告和使用redis缓存首页广告

---
+++ pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/UploadController.java
@@ -0,0 +1,34 @@
+package com.pinyougou.manager.controller;
+
+import entity.Result;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import util.FastDFSClient;
+
+/**
+ * Created by Enzo Cotter on 2018/9/3.
+ */
+@RestController
+public class UploadController {
+
+    @Value("${FILE_SERVER_URL}")
+    private String FILE_SERVER_URL;
+
+    @RequestMapping("/upload")
+    public Result upload(MultipartFile file) {
+        try {
+            //获取文件名
+            String filename = file.getOriginalFilename();
+            String extName = filename.substring(filename.lastIndexOf(".") + 1);
+            FastDFSClient fastDFSClient = new FastDFSClient("classpath:config/fdfs_client.conf");
+            String path = fastDFSClient.uploadFile(file.getBytes(), extName);
+            String urlAddress = FILE_SERVER_URL + path;
+            return new Result(true, urlAddress);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result(false, "上传失败");
+        }
+    }
+}
